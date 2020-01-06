bab7915ac72abdb383ded541bb5ff62a5e09afd1
Keith Fung <keith.robert.fung@gmail.com>
2020-01-06 15:02:35 -0500

    ✨ Mock apis for card, usb and electionguard
    Mock apis for the 3 main apis in use. These allow a user to go from start to finish using the sample election. 
    
    📦 fetch-mock and dotenv were added.
    
    🐛 2 points of contention are the way that cards function which require an exact id and the cards/usb changing connection status back and forth to allow going through all the steps

---
+++ src/mocks/electionguard/mockElectionGuardApi.ts
@@ -0,0 +1,41 @@
+import fetchMock, { MockOptionsMethodPost } from 'fetch-mock'
+import createElectionResponse from './responses/create_election_response.json'
+import recordBallotsResponse from './responses/record_ballots_response.json'
+import tallyVotesResponse from './responses/tally_votes_response.json'
+import { ElectionGuardConfig } from '../../electionguard'
+
+const removeNumberValues = (object: { [key: string]: string }, max: number) => {
+  // eslint-disable-next-line no-restricted-syntax
+  for (const key in Object.keys(object)) {
+    if (((key as unknown) as number) >= max) {
+      // eslint-disable-next-line no-param-reassign
+      delete object[key]
+    }
+  }
+  return object
+}
+
+export const mockElectionGuardApi = () => {
+  fetchMock.post('/election', (url, options: MockOptionsMethodPost) => {
+    const request = JSON.parse((options.body as unknown) as string)
+    const config = request.config as ElectionGuardConfig
+    createElectionResponse.electionGuardConfig.numberOfTrustees =
+      config.numberOfTrustees
+    createElectionResponse.electionGuardConfig.threshold = config.threshold
+    const newKeys = removeNumberValues(
+      createElectionResponse.trusteeKeys,
+      config.numberOfTrustees
+    )
+    return JSON.stringify({ ...createElectionResponse, trusteeKeys: newKeys })
+  })
+
+  fetchMock.post('/election/RecordBallots', () => {
+    return JSON.stringify(recordBallotsResponse)
+  })
+
+  fetchMock.post('/election/TallyVotes', () => {
+    return JSON.stringify(tallyVotesResponse)
+  })
+}
+
+export default mockElectionGuardApi
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="react-scripts" />
+
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    REACT_APP_MOCK_CARD_SERVER:  string
+    REACT_APP_MOCK_USB_SERVER: string
+    REACT_APP_MOCK_EG_SERVER: string
+  }
+}
